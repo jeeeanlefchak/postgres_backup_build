98992a27eaba5f2fec147f286d27a836ab92a822
jeeeanlefchak <jean_2103@hotmail.com>
2020-07-10 14:58:06 -0300

    s

--- backup_postgres.bat
+++ backup_postgres.bat
@@ -1,12 +1,10 @@
-Windows 
-
 set host=localhost
 set port=5432
 set dataBaseName=hForms
 set userName=postgres
 set password=123456
 set pathPgDump="c:\Program Files\PostgreSQL\10\bin\"
-set BACKUP_FILE=d:\backup\DataBase\%dataBaseName%_%datestr%.backup
+set pathSave=d:\backup\DataBase
 
 @Echo Off
  @For /F "tokens=1,2,3,4 delims=/ " %%A in ('Date /t') do @(
@@ -28,10 +26,10 @@ set BACKUP_FILE=d:\backup\DataBase\%dataBaseName%_%datestr%.backup
 set datestr=%All%%Allm%%AMPM%
 echo datestr is %datestr%
 
+set BACKUP_FILE=%pathSave%\%dataBaseName%_%datestr%.backup
+
 echo backup file name is %BACKUP_FILE%
 SET PGPASSWORD=%password%
 echo on
 cd %pathPgDump%
 pg_dump.exe -h  %host% -U %userName% -p %port% -F c -b -v %dataBaseName% > %BACKUP_FILE% 
-
-pause
